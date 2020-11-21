792ef3ccbffe25ac9b4b3e5998edb2b412546958
Ahmed Dini <ahmed@catch.et>
2020-11-21 09:53:51 +0000

    Refactored some JDL entities and added new ones based on the client's suggestions

--- src/main/java/com/amc/careplanner/config/ApplicationProperties.java
+++ src/main/java/com/amc/careplanner/config/ApplicationProperties.java
@@ -8,6 +8,24 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
  * Properties are configured in the {@code application.yml} file.
  * See {@link io.github.jhipster.config.JHipsterProperties} for a good example.
  */
+
 @ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
 public class ApplicationProperties {
+	private final AuthServer authServer = new AuthServer();
+	public static class AuthServer {
+
+        private String registerUrl = com.amc.careplanner.utils.Constants.AUTH_SERVER_DEFAULT_REGISTER_URL;
+
+
+        public String getRegisterUrl() {
+            return registerUrl;
+        }
+
+        public void setRegisterUrl(String registerUrl) {
+            this.registerUrl = registerUrl;
+        }
+    }
+	public AuthServer getAuthServer() {
+		return authServer;
+	}
 }
