7b08bc5227a1fdff94c86b3b57e6bdd5202b7a33
moha-iraad <muxamad128@gmail.com>
2021-01-03 12:23:54 +0300

    InvoiceResource-changes

--- src/main/java/com/amc/careplanner/web/rest/ext/InvoiceResourceExt.java
+++ src/main/java/com/amc/careplanner/web/rest/ext/InvoiceResourceExt.java
@@ -5,9 +5,16 @@ import com.amc.careplanner.web.rest.InvoiceResource;
 import com.amc.careplanner.web.rest.errors.BadRequestAlertException;
 import com.amc.careplanner.service.dto.InvoiceDTO;
 import com.amc.careplanner.service.ext.InvoiceServiceExt;
+import com.amc.careplanner.service.dto.EmployeeHolidayCriteria;
+import com.amc.careplanner.service.dto.EmployeeHolidayDTO;
 import com.amc.careplanner.service.dto.InvoiceCriteria;
+import com.amc.careplanner.domain.User;
+import com.amc.careplanner.repository.ext.UserRepositoryExt;
+import com.amc.careplanner.security.AuthoritiesConstants;
+import com.amc.careplanner.security.SecurityUtils;
 import com.amc.careplanner.service.InvoiceQueryService;
 
+import io.github.jhipster.service.filter.LongFilter;
 import io.github.jhipster.web.util.HeaderUtil;
 import io.github.jhipster.web.util.PaginationUtil;
 import io.github.jhipster.web.util.ResponseUtil;
@@ -20,11 +27,13 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.time.ZonedDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -45,26 +54,32 @@ public class InvoiceResourceExt extends InvoiceResource{
     private final InvoiceServiceExt invoiceServiceExt;
 
     private final InvoiceQueryService invoiceQueryService;
+    
+    private final UserRepositoryExt userRepositoryExt;
 
-    public InvoiceResourceExt(InvoiceServiceExt invoiceServiceExt, InvoiceQueryService invoiceQueryService) {
+    public InvoiceResourceExt(InvoiceServiceExt invoiceServiceExt, InvoiceQueryService invoiceQueryService,UserRepositoryExt userRepositoryExt) {
         super(invoiceServiceExt,invoiceQueryService);
     	this.invoiceServiceExt = invoiceServiceExt;
         this.invoiceQueryService = invoiceQueryService;
+        this.userRepositoryExt = userRepositoryExt;
     }
 
     /**
-     * {@code POST  /invoices} : Create a new invoice.
+     * {@code POST  /invoices} : Create a new invoice. 
      *
      * @param invoiceDTO the invoiceDTO to create.
      * @return the {@link ResponseEntity} with status {@code 201 (Created)} and with body the new invoiceDTO, or with status {@code 400 (Bad Request)} if the invoice has already an ID.
      * @throws URISyntaxException if the Location URI syntax is incorrect.
      */
-    @PostMapping("/invoices")
+    @PostMapping("/create-invoice-by-client-id")
     public ResponseEntity<InvoiceDTO> createInvoice(@Valid @RequestBody InvoiceDTO invoiceDTO) throws URISyntaxException {
         log.debug("REST request to save Invoice : {}", invoiceDTO);
         if (invoiceDTO.getId() != null) {
             throw new BadRequestAlertException("A new invoice cannot already have an ID", ENTITY_NAME, "idexists");
         }
+        //InvoiceDTO.setDateCreated(ZonedDateTime.now());
+        invoiceDTO.setLastUpdatedDate(ZonedDateTime.now());
+        invoiceDTO.setClientId(getClientIdFromLoggedInUser());
         InvoiceDTO result = invoiceServiceExt.save(invoiceDTO);
         return ResponseEntity.created(new URI("/api/invoices/" + result.getId()))
             .headers(HeaderUtil.createEntityCreationAlert(applicationName, true, ENTITY_NAME, result.getId().toString()))
@@ -80,12 +95,16 @@ public class InvoiceResourceExt extends InvoiceResource{
      * or with status {@code 500 (Internal Server Error)} if the invoiceDTO couldn't be updated.
      * @throws URISyntaxException if the Location URI syntax is incorrect.
      */
-    @PutMapping("/invoices")
+    @PutMapping("/update-invoice-by-client-id")
     public ResponseEntity<InvoiceDTO> updateInvoice(@Valid @RequestBody InvoiceDTO invoiceDTO) throws URISyntaxException {
         log.debug("REST request to update Invoice : {}", invoiceDTO);
         if (invoiceDTO.getId() == null) {
             throw new BadRequestAlertException("Invalid id", ENTITY_NAME, "idnull");
         }
+        if (invoiceDTO != null && invoiceDTO.getClientId() != null && invoiceDTO.getClientId() != getClientIdFromLoggedInUser()) {
+      	  throw new BadRequestAlertException("clientId mismatch", ENTITY_NAME, "clientIdMismatch");
+      }
+        invoiceDTO.setLastUpdatedDate(ZonedDateTime.now());
         InvoiceDTO result = invoiceServiceExt.save(invoiceDTO);
         return ResponseEntity.ok()
             .headers(HeaderUtil.createEntityUpdateAlert(applicationName, true, ENTITY_NAME, invoiceDTO.getId().toString()))
@@ -99,10 +118,14 @@ public class InvoiceResourceExt extends InvoiceResource{
      * @param criteria the criteria which the requested entities should match.
      * @return the {@link ResponseEntity} with status {@code 200 (OK)} and the list of invoices in body.
      */
-    @GetMapping("/invoices")
+    @GetMapping("/get-all-invoices-by-client-id")
     public ResponseEntity<List<InvoiceDTO>> getAllInvoices(InvoiceCriteria criteria, Pageable pageable) {
         log.debug("REST request to get Invoices by criteria: {}", criteria);
-        Page<InvoiceDTO> page = invoiceQueryService.findByCriteria(criteria, pageable);
+        InvoiceCriteria invoiceCriteria = new  InvoiceCriteria();
+		LongFilter longFilterForClientId = new LongFilter();
+		longFilterForClientId.setEquals(getClientIdFromLoggedInUser());
+		 invoiceCriteria.setClientId(longFilterForClientId);
+        Page<InvoiceDTO> page = invoiceQueryService.findByCriteria(invoiceCriteria, pageable);
         HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(ServletUriComponentsBuilder.fromCurrentRequest(), page);
         return ResponseEntity.ok().headers(headers).body(page.getContent());
     }
@@ -125,7 +148,7 @@ public class InvoiceResourceExt extends InvoiceResource{
      * @param id the id of the invoiceDTO to retrieve.
      * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the invoiceDTO, or with status {@code 404 (Not Found)}.
      */
-    @GetMapping("/invoices/{id}")
+    @GetMapping("/get-invoices-by-client-id/{id}")
     public ResponseEntity<InvoiceDTO> getInvoice(@PathVariable Long id) {
         log.debug("REST request to get Invoice : {}", id);
         Optional<InvoiceDTO> invoiceDTO = invoiceServiceExt.findOne(id);
@@ -138,10 +161,23 @@ public class InvoiceResourceExt extends InvoiceResource{
      * @param id the id of the invoiceDTO to delete.
      * @return the {@link ResponseEntity} with status {@code 204 (NO_CONTENT)}.
      */
-    @DeleteMapping("/invoices/{id}")
+    @DeleteMapping("/delete-invoices-by-client-id/{id}")
+    @PreAuthorize("hasAuthority(\"" + AuthoritiesConstants.COMPANY_ADMIN + "\")")
     public ResponseEntity<Void> deleteInvoice(@PathVariable Long id) {
         log.debug("REST request to delete Invoice : {}", id);
         invoiceServiceExt.delete(id);
         return ResponseEntity.noContent().headers(HeaderUtil.createEntityDeletionAlert(applicationName, true, ENTITY_NAME, id.toString())).build();
+    }
+
+    private Long getClientIdFromLoggedInUser() {
+    	Long clientId = 0L;
+    	String loggedInAdminUserEmail = SecurityUtils.getCurrentUserLogin().get();
+		User loggedInAdminUser = userRepositoryExt.findOneByEmailIgnoreCase(loggedInAdminUserEmail).get();
+		
+		if(loggedInAdminUser != null) {
+			clientId = Long.valueOf(loggedInAdminUser.getLogin());
+		}
+		
+		return clientId;
     }
 }
